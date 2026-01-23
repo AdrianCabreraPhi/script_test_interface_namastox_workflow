7d0a76077ad4777bafb785b97b9a3d8c0f04230f
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-01-23 10:12:36 +0100

    improved code: implemented credentials in .env

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
-import pytest
+import pytest,os
 from getpass import getpass
 from playwright.sync_api import Page, expect
+from dotenv import load_dotenv
 
-USERNAME = input("Introduce your username: ")
-PASSWORD = getpass(prompt="Introduce your password: ")
+load_dotenv()
 
+USERNAME = os.getenv("NAMASTOX_USER")
+PASSWORD = os.getenv("NAMASTOX_PASSWORD")
+
+if USERNAME is None or PASSWORD is None:
+    print("Not found environment variables in your directory")
+    print("Recommendation: configure .env with your credentials")
+    USERNAME = input("Introduce your username: ")
+    PASSWORD = getpass(prompt="Introduce your password: ")
 
 @pytest.fixture(scope="function")
 def auth_page(page: Page):
